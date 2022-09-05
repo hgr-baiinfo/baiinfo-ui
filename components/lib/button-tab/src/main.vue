b91d0674b4e9507c04ce7bbcfeec858e65987ba8
huangguiru <2168638306@qq.com>
2022-09-05 16:11:35 +0800

    update

--- components/lib/button-tab/src/main.vue
+++ components/lib/button-tab/src/main.vue
@@ -1,14 +1,14 @@
 <template>
-  <div class="bi-button-tab" :style="{width:width}" >
-      <span
-              v-for="(item, index) in list"
-              :key="index"
-              class="tab-item"
-              @click="change(item, index)"
-              :class="[`${currentIndex == index ? 'current' : 'hovered'}`,`${type}`]"
-          >
-        {{ item.label }}
-      </span>
+  <div class="bi-button-tab" :style="{ width: width }">
+    <span
+      v-for="(item, index) in list"
+      :key="index"
+      class="tab-item"
+      @click="change(item, index)"
+      :class="[`${currentIndex == index ? 'current' : 'hovered'}`, `${type}`]"
+    >
+      {{ item.label }}
+    </span>
   </div>
 </template>
 <script>
@@ -20,13 +20,13 @@ export default {
     };
   },
   props: {
-    type:{
-      type:String,
-      default:'default'
+    type: {
+      type: String,
+      default: "default",
     },
-    width:{
-      type:String,
-      default:'140px'
+    width: {
+      type: String,
+      default: "140px",
     },
     list: {
       type: Array,
@@ -38,68 +38,70 @@ export default {
   methods: {
     change(item, index) {
       this.currentIndex = index;
-      this.$emit("change", item.value,item);
+      this.$emit("change", item.value, item);
     },
   },
 };
 </script>
 <style lang="scss" scoped>
-.bi-button-tab{
+.bi-button-tab {
   display: flex;
   justify-content: space-between;
   border-collapse: collapse;
   display: flex;
   justify-content: space-between;
   border-collapse: collapse;
-  .tab-item {
-    height: 40px;
-    line-height: 36px;
-    box-sizing: border-box;
-    border: 1px solid #dcdfe6;
-    cursor: pointer;
-    flex: 1;
-    padding-left: 4px;
-    padding-right: 4px;
-    align-items: center;
-    text-align: center;
-    font-size: 14px;
-    font-family: SourceHanSansCN-Regular;
-    font-weight: 400;
-    color: #333333;
-    box-sizing: border-box;
-    border-collapse: collapse;
-    border-left: none;
-    min-width: 70px;
-    background: #fff;
-    &:first-of-type {
-      border-left: 1px solid #dcdfe6;
-    }
-  }
-  .current {
-    background: #c43233;
-    color: #fff;
-    &:first-of-type {
-      border-left: 1px solid #c43233;
-    }
-  }
-  .small{
-    height: 30px;
-    line-height: 27px;
-  }
-  .hovered:hover {
-    color: #c43233;
-    background:#fff;
-  }
-  .primary.current {
-    border:1px solid #C43233;
-    background:#FFF8F8;
-    color:#C43233;
-  }
-  .current:first-of-type{
-    border-left:none;
-  }
-  .primary.current:first-of-type{
-    border-left:1px solid #C43233;;
-  }
+}
+.bi-button-tab .tab-item {
+  height: 40px;
+  line-height: 36px;
+  box-sizing: border-box;
+  border: 1px solid #dcdfe6;
+  cursor: pointer;
+  flex: 1;
+  padding-left: 4px;
+  padding-right: 4px;
+  align-items: center;
+  text-align: center;
+  font-size: 14px;
+  font-family: SourceHanSansCN-Regular;
+  font-weight: 400;
+  color: #333333;
+  box-sizing: border-box;
+  border-collapse: collapse;
+  border-left: none;
+  min-width: 70px;
+  background: #fff;
+}
+.bi-button-tab .tab-item:first-of-type {
+  border-left: 1px solid #dcdfe6;
+}
+.bi-button-tab .current {
+  background: #c43233;
+  color: #fff;
+}
+.bi-button-tab .current:first-of-type {
+  border-left: 1px solid #c43233;
+}
+.bi-button-tab .small {
+  height: 30px;
+  line-height: 27px;
+}
+
+.bi-button-tab .hovered:hover {
+  color: #c43233;
+  background: #fff;
+}
+.bi-button-tab .primary.current {
+  border: 1px solid #c43233;
+  background: #fff8f8;
+  color: #c43233;
+}
+
+.bi-button-tab .current:first-of-type {
+  border-left: none;
+}
+.bi-button-tab .primary.current:first-of-type {
+  border-left: 1px solid #c43233;
 }
 </style>
