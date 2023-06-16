92b94345e71972e6b0773978b78093b34befe2dd
huangguiru <2168638306@qq.com>
2023-06-16 18:00:47 +0800

    调整样式

--- components/lib/news-dialog/src/main.vue
+++ components/lib/news-dialog/src/main.vue
@@ -10,55 +10,65 @@
   >
     <div v-loading="loading">
       <div class="dialog-header">
-        <div class="left">
-          <span>{{ dialogInfo[mergedProps.source] }}</span>
-          <span>{{ dialogInfo[mergedProps.addTime] }}</span>
-          <span>{{ dialogInfo[mergedProps.keywords] }}</span>
+        <div class="left gray">
+          {{ dialogInfo[mergedProps.category] }}
         </div>
         <div class="right">
-          <span v-if="showCount && dialogInfo[mergedProps.browseCount]"
-            >{{ dialogInfo[mergedProps.browseCount] }}&nbsp;&nbsp;阅读</span
-          >
           <img :src="closeImg" alt="" class="close-img" @click="close" />
         </div>
       </div>
       <div class="dialog-content">
-        <div class="title">
-          <span v-html="dialogInfo[mergedProps.newsTitle]"></span>
-          <span class="operation">
-            <img
-              :src="shareImg"
-              class="share"
-              v-if="showShare"
-              @click="shareFn"
-            />
-            <img
-              :src="collectionSrc"
-              alt=""
-              v-if="showCollection"
-              class="star"
-              @click="toggleCollection(dialogInfo)"
-            />
-          </span>
+        <div class="content-top">
+          <div class="title">
+            <span v-html="dialogInfo[mergedProps.newsTitle]"></span>
+            <span class="operation">
+              <img
+                :src="shareImg"
+                class="share"
+                v-if="showShare"
+                @click="shareFn"
+              />
+              <img
+                :src="collectionSrc"
+                alt=""
+                v-if="showCollection"
+                class="star"
+                @click="toggleCollection(dialogInfo)"
+              />
+            </span>
+          </div>
+          <div class="source">
+            <span>来源：{{ dialogInfo[mergedProps.source] }}</span>
+            <span>更新时间：{{ dialogInfo[mergedProps.addTime] }}</span>
+            <span>关键词：{{ dialogInfo[mergedProps.keywords] }}</span>
+          </div>
         </div>
         <div class="content">
           <div ref="content" v-html="dialogInfo[mergedProps.content]"></div>
         </div>
       </div>
-      <div class="dialog-footer">
+      <div class="bottom-wrap" :class="{ haspre: showPreAndNext }">
         <p class="tips">声明：信息仅供参考，据此操作责任自负</p>
-        <div class="close-btn"><span @click="close">关闭</span></div>
-        <div class="pre-next" v-if="showPreAndNext">
-          <p class="link" :class="{ gray: preText == '无' }">
-            上一篇：<span @click="link('pre', dialogInfo)">{{ preText }}</span>
-          </p>
-          <p class="link" :class="{ gray: nextText == '无' }">
-            下一篇：<span @click="link('next', dialogInfo)">{{
-              nextText
-            }}</span>
-          </p>
+        <div class="next-pre" v-if="showPreAndNext">
+          <div class="pre-next">
+            <p class="link" :class="{ gray: preText == '无' }">
+              <span class="triangle-up"></span>
+              上一篇：<span @click="link('pre', dialogInfo)">{{
+                preText
+              }}</span>
+            </p>
+            <p class="link" :class="{ gray: nextText == '无' }">
+              <span class="triangle-down"></span>
+              下一篇：<span @click="link('next', dialogInfo)">{{
+                nextText
+              }}</span>
+            </p>
+          </div>
         </div>
         <div v-else class="footer-placeholder"></div>
+        <div class="dialog-footer">
+          <div class="close-btn"><span @click="close">关闭</span></div>
+        </div>
       </div>
     </div>
   </el-dialog>
@@ -104,6 +114,7 @@ export default {
           newsTitleUp: "",
           newsIdDown: "",
           newsTitleDown: "",
+          category: "",
         };
       },
     },
@@ -173,6 +184,7 @@ export default {
           newsTitleUp: "newsTitleUp",
           newsIdDown: "newsIdDown",
           newsTitleDown: "newsTitleDown",
+          category: "category",
         },
         this.defaultProps
       );
